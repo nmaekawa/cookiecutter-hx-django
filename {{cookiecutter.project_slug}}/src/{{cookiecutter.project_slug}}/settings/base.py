f75b5fcaee788dca4d913d877bfcd484ca744692
nmaekawa <nmaekawa@g.harvard.edu>
2021-08-06 17:42:02 -0400

    default_auto_field added to be par with settings generated by django-admin

--- {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/settings/base.py
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/settings/base.py
@@ -126,26 +126,25 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
-
 LANGUAGE_CODE = "en-us"
-
 TIME_ZONE = "UTC"
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
 STATIC_URL = "/static/"
 STATIC_ROOT = os.environ.get(
     "{{cookiecutter.project_prefix}}_STATIC_ROOT",
     os.path.join(BASE_DIR, "http_static/"),
 )
+
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
 # Logging config
